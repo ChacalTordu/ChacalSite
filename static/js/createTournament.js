e30b6499076f0e5ab0ae0f8ef71257194be99ae6
ChacalTordu <leo.lebrun04@gmail.com>
2023-06-12 22:44:33 +0200

    progressing in creating tournament

--- static/js/createTournament.js
+++ static/js/createTournament.js
@@ -1,147 +1,103 @@
 function createTournament(button) {
-    button.classList.add('hidden');
-    // Charger le fichier JSON contenant les données du tournoi
-    fetch("/tournoi.json")
-      .then(response => response.json())
-      .then(data => {
-        const tournoi = data[0]; // Supposant que le fichier JSON contient un tableau avec un seul objet tournoi
-        const participants = tournoi.participants;
-        createTournamentStructure(participants);
-      })
-      .catch(error => {
-        console.log("Une erreur s'est produite lors du chargement du fichier JSON :", error);
-      });
-  }
-  
+  button.classList.add('hidden');
+  // Charger le fichier JSON contenant les données du tournoi
+  fetch("/tournoi.json")
+    .then(response => response.json())
+    .then(data => {
+      const tournoi = data[0]; // Supposant que le fichier JSON contient un tableau avec un seul objet tournoi
+      const participants = tournoi.participants;
+      createTournamentStructure(participants);
+    })
+    .catch(error => {
+      console.log("Une erreur s'est produite lors du chargement du fichier JSON :", error);
+    });
+}
+
 // Fonction pour créer la structure du tournoi en fonction du nombre de participants
 function createTournamentStructure(participants) {
-    const etatTournoi = document.querySelector('.etatTournoi.theme.theme-dark.visible');
-  
-    const bracket = document.createElement("div");
-    bracket.className = "bracket";
-    etatTournoi.appendChild(bracket);
-  
-    const firstColumn = document.createElement("div");
-    firstColumn.className = "column column-1";
-    bracket.appendChild(firstColumn);
-  
-    for (let i = 0; i < participants.length; i++) {
-      const team = createTeam(participants[i]);
-      firstColumn.appendChild(team);
-    }
-  
-    for (let i = 1; i < Math.log2(participants.length); i++) {
-      const column = document.createElement("div");
-      column.className = "column column-" + (i + 1); // Ajout du numéro de colonne à la classe
-      bracket.appendChild(column);
-  
-      // Ajoutez ici le code pour créer les matchs dans les colonnes suivantes si nécessaire
+  const numTeams = participants.length;
+  const numMatches = numTeams / 2;
+
+  const bracket = document.createElement("div");
+  bracket.className = "bracket";
+
+  for (let i = 0; i < 3; i++) {
+    const column = document.createElement("div");
+    column.className = `column column-${i + 1}`;
+    bracket.appendChild(column);
+
+    const matchesInColumn = i === 0 ? numMatches : Math.pow(2, 3 - i - 1);
+
+    for (let j = 0; j < matchesInColumn; j++) {
+      const match = createMatch(column, participants, i, j);
+      column.appendChild(match);
     }
   }
-  
-  // Fonction pour créer un élément représentant une équipe
-  function createTeam(teamName) {
-    const teamElement = document.createElement("div");
-    teamElement.className = "team";
-    teamElement.textContent = teamName;
-    return teamElement;
+
+  const createTournamentButton = document.querySelector(".createTournament");
+  createTournamentButton.insertAdjacentElement("afterend", bracket);
+}
+
+// Fonction pour créer un match avec les noms des participants
+function createMatch(column, participants, columnIndex, matchIndex) {
+  const match = document.createElement("div");
+  match.className = "match";
+
+  if (columnIndex === 0) {
+    const participantIndex1 = matchIndex * 2;
+    const participantIndex2 = matchIndex * 2 + 1;
+    const winnerClass = getWinnerClass(participants, participantIndex1, participantIndex2);
+
+    const topTeam = createTeam(participants[participantIndex1]);
+    const bottomTeam = createTeam(participants[participantIndex2]);
+
+    match.classList.add(winnerClass);
+    match.appendChild(topTeam);
+    match.appendChild(bottomTeam);
   }
-//   // Fonction pour créer la structure du tournoi en fonction du nombre de participants
-//   function createTournamentStructure(participants) {
-//     const etatTournoi = document.querySelector('.etatTournoi.theme.theme-dark.visible');
-  
-//     const bracket = document.createElement("div");
-//     bracket.className = "bracket";
-//     etatTournoi.appendChild(bracket);
-    
-//     // creer le nombre de column adequat
-//     //for (let i = 0; i < participants.length; i++) {
-//     for (let i = 0; i < Math.log2(participants.length); i++) {
-//         const column = document.createElement("div");
-//         column.className = "column column-" + (i + 1); // Ajout du numéro de colonne à la classe
-//         bracket.appendChild(column);
-      
-//         if (i === 0) {
-//           for (let j = 0; j < participants[i].length; j++) {
-//             const match = createMatch("undefined", participants[i][j]);
-//             column.appendChild(match);
-//           }
-//         }
-//     }  
-//   }
-  
-  // Fonction pour créer un match avec les noms des participants
-  function createMatch(winnerClass, participants) {
-    const match = document.createElement("div");
-    match.className = "match " + winnerClass;
-  
-    for (let i = 0; i < participants.length; i++) {
-      const team = createTeam(participants[i]);
-      match.appendChild(team);
-    }
-  
-    const matchLines = document.createElement("div");
-    matchLines.className = "match-lines";
-    match.appendChild(matchLines);
-  
-    const line1 = document.createElement("div");
-    line1.className = "line one";
-    matchLines.appendChild(line1);
-  
-    const line2 = document.createElement("div");
-    line2.className = "line two";
-    matchLines.appendChild(line2);
-  
-    return match;
+
+  const matchLines = document.createElement("div");
+  matchLines.className = "match-lines";
+  match.appendChild(matchLines);
+
+  const line1 = document.createElement("div");
+  line1.className = "line one";
+  matchLines.appendChild(line1);
+
+  const line2 = document.createElement("div");
+  line2.className = "line two";
+  matchLines.appendChild(line2);
+
+  if (columnIndex === 0) {
+    const matchLinesAlt = document.createElement("div");
+    matchLinesAlt.className = "match-lines alt";
+    match.appendChild(matchLinesAlt);
+
+    const altLine1 = document.createElement("div");
+    altLine1.className = "line one";
+    matchLinesAlt.appendChild(altLine1);
   }
-  
-  // Fonction pour créer une équipe avec le nom du participant
-  function createTeam(name) {
-    const team = document.createElement("div");
-    team.className = "match-top team";
-  
-    const nameSpan = document.createElement("span");
-    nameSpan.className = "name";
-    nameSpan.textContent = name;
-    team.appendChild(nameSpan);
-  
-    return team;
-  }
-  
-  // Fonction pour mettre à jour les scores des matchs
-  function updateMatchScores(match, topScore, bottomScore) {
-    const scoreSpans = match.getElementsByClassName("score");
-  
-    if (scoreSpans.length === 2) {
-      scoreSpans[0].textContent = topScore;
-      scoreSpans[1].textContent = bottomScore;
-    }
-  }
-  
-  // Fonction pour récupérer les scores des matchs et afficher le gagnant
-  function getMatchScoresAndDisplayWinner(columnOne) {
-    const matches = columnOne.getElementsByClassName("match");
-    const winners = [];
-  
-    for (let i = 0; i < matches.length; i++) {
-      const topScore = parseInt(matches[i].querySelector(".match-top .score").textContent);
-      const bottomScore = parseInt(matches[i].querySelector(".match-bottom .score").textContent);
-  
-      if (topScore > bottomScore) {
-        winners.push(matches[i].querySelector(".match-top .name").textContent);
-      } else if (bottomScore > topScore) {
-        winners.push(matches[i].querySelector(".match-bottom .name").textContent);
-      } else {
-        winners.push("Tie");
-      }
-    }
-  
-    const columnTwo = document.querySelector(".column.two");
-    columnTwo.innerHTML = "";
-  
-    for (let i = 0; i < winners.length; i++) {
-      const match = createMatch("undefined", [winners[i]]);
-      columnTwo.appendChild(match);
-    }
-  }
-  
+
+  return match;
+}
+
+// Fonction pour créer une équipe avec le nom du participant
+function createTeam(name) {
+  const team = document.createElement("div");
+  team.className = "match-top team";
+
+  const nameSpan = document.createElement("span");
+  nameSpan.className = "name";
+  nameSpan.textContent = name;
+  team.appendChild(nameSpan);
+
+  return team;
+}
+
+// Fonction pour obtenir la classe du gagnant du match
+function getWinnerClass(participants, index1, index2) {
+  const score1 = participants[index1].score || 0;
+  const score2 = participants[index2].score || 0;
+
+  return score1 > score2 ? "winner-top" : "winner-bottom";
+}
