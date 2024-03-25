fbeadd45cc292610779a9f86352d123d70d41864
Leo LE-BRUN <leo.le-brun@apside.com>
2024-03-25 17:00:32 +0100

    [BUG] : players page superposition

---
+++ src/components/zone/zonePlayerPresentation.vue
@@ -0,0 +1,52 @@
+<template>
+    <div class="player">
+        <div class="media"><img :src="media"></div>
+        <h2 class="description">{{ description }}</h2>
+        <div class="name">{{ name }}</div>
+    </div>
+</template>
+
+<script setup>
+    const props = defineProps({media : String, description : String, name : String})
+</script>
+
+<style scoped>
+.player {
+    width: 390px;
+    margin: 0 !important;
+    position: absolute;
+    top: 138px;
+    left: 0px;
+    overflow: hidden;
+    flex-shrink: 0;
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+    justify-content: flex-start;
+    padding: var(--padding-2xs) var(--padding-3xs);
+    box-sizing: border-box;
+    gap: var(--gap-xl);
+}
+.media {
+    align-self: stretch;
+    height: 370px;
+    position: relative;
+    background-color: var(--color-black);
+}
+.description {
+    margin: 0;
+    align-self: stretch;
+    position: relative;
+    font-size: inherit;
+    line-height: 135.94%;
+    font-weight: 400;
+    font-family: inherit;
+    text-shadow: 0px 31.3px 62.53px rgba(0, 0, 0, 0.64);
+}
+.name {
+    align-self: stretch;
+    position: relative;
+    font-size: var(--font-size-sm);
+    line-height: 135.94%;
+  }
+</style>
--- src/pages/players.vue
+++ src/pages/players.vue
@@ -5,54 +5,36 @@
     <h1 class="title">Chacal Ligue - Joueurs</h1>
     <section class="joueurs-parent">
       <div class="joueurs">
-        <div class="antoine">
-          <div class="media" />
-          <h2 class="title1">
-            Homme très dangereux. Capable de l’impossibe, comme du pire. Son
-            style de jeu peut surprendre ...
-          </h2>
-          <div class="subtitle">Tonio Le Connard</div>
-        </div>
-        <div class="gwendal">
-          <div class="media1" />
-          <h2 class="title2">
-            Savant, sage, aucun défis ne lui fait perdre son sang froid. A
-            surveiller ...
-          </h2>
-          <div class="subtitle1">Transpalet</div>
-        </div>
-        <div class="marius">
-          <div class="media2" />
-          <h2 class="title3">
-            Sa capacité à être dérangé, peut aussi déranger toute la ligue.
-            Très fort ce pugzin ...
-          </h2>
-          <div class="subtitle2">Pugzindecong</div>
-        </div>
-        <div class="pierre">
-          <div class="media3" />
-          <h2 class="title4">
-            Ce spécialiste de la défonce, peut défoncer les adversaires en un
-            claquement de petard ...
-          </h2>
-          <div class="subtitle3">Pierromax</div>
-        </div>
-        <div class="lo">
-          <div class="media4" />
-          <h2 class="title5">
-            Le chacal est capable de grande ruse, attention aux chacals coup
-            dans le dos ...
-          </h2>
-          <div class="subtitle4">ChacalTordu</div>
-        </div>
-        <div class="julien">
-          <div class="media5" />
-          <h2 class="title6">
-            Ce spécialiste de la maîtrise du corp humain, maîtrise tout aussi
-            bien l’art du jeu vidéo ... Incroyable
-          </h2>
-          <div class="subtitle5">Wazzakunga</div>
-        </div>
+        <zonePlayerPresentation 
+          media="" 
+          description=" Homme très dangereux. Capable de l’impossibe, comme du pire. Son
+                        style de jeu peut surprendre ..." 
+          name="Tonio Le Connard"/>
+          <zonePlayerPresentation 
+          media="" 
+          description=" Savant, sage, aucun défis ne lui fait perdre son sang froid. A
+                        surveiller ..." 
+          name="Transpalet"/>
+          <zonePlayerPresentation 
+          media="" 
+          description=" Sa capacité à être dérangé, peut aussi déranger toute la ligue.
+                        Très fort ce pugzin ..." 
+          name="Pugzindecong"/>
+          <zonePlayerPresentation 
+          media="" 
+          description=" Ce spécialiste de la défonce, peut défoncer les adversaires en un
+                        claquement de petard ..." 
+          name="Pierromax"/>
+          <zonePlayerPresentation 
+          media="" 
+          description=" Le chacal est capable de grande ruse, attention aux chacals coup
+                        dans le dos ..." 
+          name="ChacalTordu"/>
+          <zonePlayerPresentation 
+          media="" 
+          description=" Ce spécialiste de la maîtrise du corp humain, maîtrise tout aussi
+                        bien l’art du jeu vidéo ... Incroyable" 
+          name="Wazzakunga"/>
       </div>
       <div class="footer-instance">
         <router-link class="router-link" to="/"><ButtonBlack textButton="Classement"/></router-link>
@@ -67,6 +49,7 @@
   import ZoneNavbar from "../components/zone/zoneNavbar.vue";
   import ZoneFooter from "../components/zone/zoneFooter.vue";
   import ButtonBlack from "../components/buttons/buttonBlack.vue";
+  import zonePlayerPresentation from "../components/zone/zonePlayerPresentation.vue";
 </script>
 
 <style scoped>
@@ -79,231 +62,6 @@
     font-family: inherit;
     display: inline-block;
     max-width: 100%;
-  }
-  .media {
-    align-self: stretch;
-    height: 370px;
-    position: relative;
-    background-color: var(--color-black);
-  }
-  .title1 {
-    margin: 0;
-    align-self: stretch;
-    position: relative;
-    font-size: inherit;
-    line-height: 135.94%;
-    font-weight: 400;
-    font-family: inherit;
-    text-shadow: 0px 31.3px 62.53px rgba(0, 0, 0, 0.64);
-  }
-  .subtitle {
-    align-self: stretch;
-    position: relative;
-    font-size: var(--font-size-sm);
-    line-height: 135.94%;
-  }
-  .antoine {
-    width: 390px;
-    margin: 0 !important;
-    position: absolute;
-    top: 138px;
-    left: 0px;
-    overflow: hidden;
-    flex-shrink: 0;
-    display: flex;
-    flex-direction: column;
-    align-items: flex-start;
-    justify-content: flex-start;
-    padding: var(--padding-2xs) var(--padding-3xs);
-    box-sizing: border-box;
-    gap: var(--gap-xl);
-  }
-  .media1 {
-    align-self: stretch;
-    height: 370px;
-    position: relative;
-    background-color: var(--color-black);
-  }
-  .title2 {
-    margin: 0;
-    align-self: stretch;
-    height: 101px;
-    position: relative;
-    font-size: inherit;
-    line-height: 135.94%;
-    font-weight: 400;
-    font-family: inherit;
-    display: inline-block;
-  }
-  .subtitle1 {
-    align-self: stretch;
-    position: relative;
-    font-size: var(--font-size-sm);
-    line-height: 135.94%;
-  }
-  .gwendal {
-    width: 390px;
-    margin: 0 !important;
-    position: absolute;
-    top: 138px;
-    left: 435px;
-    overflow: hidden;
-    flex-shrink: 0;
-    display: flex;
-    flex-direction: column;
-    align-items: flex-start;
-    justify-content: flex-start;
-    padding: var(--padding-3xs);
-    box-sizing: border-box;
-    gap: var(--gap-xl);
-  }
-  .media2 {
-    align-self: stretch;
-    height: 370px;
-    position: relative;
-    background-color: var(--color-black);
-  }
-  .title3 {
-    margin: 0;
-    align-self: stretch;
-    position: relative;
-    font-size: inherit;
-    line-height: 135.94%;
-    font-weight: 400;
-    font-family: inherit;
-  }
-  .subtitle2 {
-    align-self: stretch;
-    position: relative;
-    font-size: var(--font-size-sm);
-    line-height: 135.94%;
-  }
-  .marius {
-    width: 390px;
-    margin: 0 !important;
-    position: absolute;
-    top: 138px;
-    left: 870px;
-    overflow: hidden;
-    flex-shrink: 0;
-    display: flex;
-    flex-direction: column;
-    align-items: flex-start;
-    justify-content: flex-start;
-    padding: var(--padding-2xs) var(--padding-3xs);
-    box-sizing: border-box;
-    gap: var(--gap-xl);
-  }
-  .media3 {
-    align-self: stretch;
-    height: 370px;
-    position: relative;
-    background-color: var(--color-black);
-  }
-  .title4 {
-    margin: 0;
-    align-self: stretch;
-    position: relative;
-    font-size: inherit;
-    line-height: 135.94%;
-    font-weight: 400;
-    font-family: inherit;
-  }
-  .subtitle3 {
-    align-self: stretch;
-    position: relative;
-    font-size: var(--font-size-sm);
-    line-height: 135.94%;
-  }
-  .pierre {
-    width: 390px;
-    margin: 0 !important;
-    position: absolute;
-    top: 688px;
-    left: 0px;
-    overflow: hidden;
-    flex-shrink: 0;
-    display: flex;
-    flex-direction: column;
-    align-items: flex-start;
-    justify-content: flex-start;
-    padding: var(--padding-2xs) var(--padding-3xs);
-    box-sizing: border-box;
-    gap: var(--gap-xl);
-  }
-  .media4 {
-    align-self: stretch;
-    height: 370px;
-    position: relative;
-    background-color: var(--color-black);
-  }
-  .title5 {
-    margin: 0;
-    align-self: stretch;
-    position: relative;
-    font-size: inherit;
-    line-height: 135.94%;
-    font-weight: 400;
-    font-family: inherit;
-  }
-  .subtitle4 {
-    align-self: stretch;
-    position: relative;
-    font-size: var(--font-size-sm);
-    line-height: 135.94%;
-  }
-  .lo {
-    width: 390px;
-    margin: 0 !important;
-    position: absolute;
-    top: 688px;
-    left: 435px;
-    overflow: hidden;
-    flex-shrink: 0;
-    display: flex;
-    flex-direction: column;
-    align-items: flex-start;
-    justify-content: flex-start;
-    padding: var(--padding-2xs) var(--padding-3xs);
-    box-sizing: border-box;
-    gap: var(--gap-xl);
-  }
-  .media5 {
-    align-self: stretch;
-    height: 370px;
-    position: relative;
-    background-color: var(--color-black);
-  }
-  .title6 {
-    margin: 0;
-    align-self: stretch;
-    position: relative;
-    font-size: inherit;
-    line-height: 135.94%;
-    font-weight: 400;
-    font-family: inherit;
-  }
-  .subtitle5 {
-    align-self: stretch;
-    position: relative;
-    font-size: var(--font-size-sm);
-    line-height: 135.94%;
-  }
-  .julien {
-    width: 390px;
-    margin: 0 !important;
-    position: absolute;
-    top: 688px;
-    left: 870px;
-    overflow: hidden;
-    flex-shrink: 0;
-    display: flex;
-    flex-direction: column;
-    align-items: flex-start;
-    justify-content: flex-start;
-    padding: var(--padding-2xs) var(--padding-3xs);
-    box-sizing: border-box;
-    gap: var(--gap-xl);
   }
   .joueurs {
     align-self: stretch;
